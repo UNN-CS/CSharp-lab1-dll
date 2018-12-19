3e6d074399b9fbce77e4a0b288b29f6a14a524a0
vanroj <vanroj5@yandex.ru>
2018-12-19 15:37:02 +0300

    Получше будет

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FromDLL1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CheckPrime";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CheckNextPrime";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(446, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "CheckPrimeN";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(446, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(186, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 127);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Prime";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(620, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "Prime";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(653, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 74);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Build a diagram";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(828, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(135, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(828, 127);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(647, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 33);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Left border";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(647, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 33);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Right border";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ src/ProgramC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace CheckDll
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\Ivan\source\repos\Lab sh1\Debug\Lab sh1.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern double Add(double x, double y);
+        [DllImport(@"C:\Users\Ivan\source\repos\Lab sh1\Debug\Lab sh1.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern void mas(int[] ts, int z);
+        [DllImport(@"C:\Users\Ivan\source\repos\Lab sh1\Debug\Lab sh1.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int isPrime(long number);
+        [DllImport(@"C:\Users\Ivan\source\repos\Lab sh1\Debug\Lab sh1.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern ulong nextPrime(ulong number);
+        [DllImport(@"C:\Users\Ivan\source\repos\Lab sh1\Debug\Lab sh1.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern ulong nPrime(int n);
+        static void Main(string[] args)
+        {
+            int m = 101;
+            int max = 20;
+            int[] ts = new int[max];
+            mas(ts, max);
+            for(int i = 0; i < max; i++)
+            {
+                Console.Write(ts[i]+" ");
+            }
+            Console.WriteLine(Add(3, 2));
+            if (isPrime(m) == 0)
+                Console.WriteLine("Не простое чилсо");
+            else
+                Console.WriteLine("Простое число");
+            Console.Write((int)nextPrime(97)+" ");
+            Console.Write((int)nPrime(100));
+
+        }
+        
+    }
+}
