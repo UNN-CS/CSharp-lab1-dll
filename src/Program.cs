531419ed7bf63007fc3c389da268206dd241b5f4
FarSaver <45992417+FarSaver@users.noreply.github.com>
2018-12-19 14:07:49 +0300

    комментарий

---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace CheckDll
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\student\Documents\Visual Studio 2017\project\Project2\Debug\TestDll.dll", CallingConvention = CallingConvention.Cdecl )]
+        static extern double Add(double x, double y);
+        [DllImport(@"C:\Users\student\Documents\Visual Studio 2017\project\Project2\Debug\TestDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern double Mass(int []x, int n);
+        [DllImport(@"C:\Users\student\Documents\Visual Studio 2017\project\Project2\Debug\TestDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int isPrime(ulong number);
+        [DllImport(@"C:\Users\student\Documents\Visual Studio 2017\project\Project2\Debug\TestDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern ulong nextPrime(ulong number);
+        [DllImport(@"C:\Users\student\Documents\Visual Studio 2017\project\Project2\Debug\TestDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern ulong nPrime(int n);
+        static void Main(string[] args)
+        {
+            int n = 10;
+            ulong num = 2;
+            ulong z = 13;
+            int v = 7;
+            int[] a = new int[n];
+            Mass(a, n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            if (isPrime(num) == 1)
+                Console.WriteLine("prost", "\n");
+                    else
+                 Console.WriteLine("ne prost","\n");
+            Console.WriteLine((int)nextPrime(z));
+            Console.WriteLine("\n");
+            Console.WriteLine((int)nPrime(v));
+            Console.WriteLine("\n");
+            Console.WriteLine(Add(3, 2));
+        }
+       
+      
+    }
+}
---
+++ src/TestDll.cpp
@@ -0,0 +1,49 @@
+#include "TestDll.h"
+#include <time.h>
+#include  <iostream>
+
+using namespace std;
+extern "C"
+{
+	_declspec(dllexport) double Add(double x, double y)
+	{
+		return x + y;
+	}
+	_declspec(dllexport) double Mass(int x[], int n)
+	{
+		srand(time_t(0));
+		for (int i = 0; i < n; i++)
+		{
+			x[i] = rand() % 100;
+
+		}
+		return 0;
+	}
+	_declspec(dllexport) int isPrime(int number)
+	{
+		for (int i = 2; i < number; i++)
+		{
+			if ((number%i == 0))
+				return 0;
+		}
+		return 1;
+	}
+	_declspec(dllexport) int unsigned long  nextPrime(unsigned long number)
+	{
+		for (int i = 0; i < INFINITY; i++)
+		{
+			number++;
+			if (isPrime(number) == 1)
+				return number;
+		}
+	}
+	_declspec(dllexport) int unsigned long nPrime(int n)
+	{
+		unsigned long prost1 = 2;
+		for (int i = 1; i < n; i++)
+		{
+			prost1 = nextPrime(prost1);	
+        }
+		return prost1;
+	}
+}
