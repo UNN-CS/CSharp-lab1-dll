ac284cb43220227f31e2652e88965605d275138a
aYellowa <golo400@icloud.com>
2018-12-20 00:28:59 +0300

    1

---
+++ Num.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <cstdlib>
+
+
+extern "C" {
+	_declspec(dllexport) double Add(double x, double y);
+	_declspec(dllexport) void fullrandom(int* array, int num);
+	_declspec(dllexport) bool isPrime(unsigned long a);
+	_declspec(dllexport) unsigned long nextPrime(unsigned long a);
+	_declspec(dllexport) unsigned long nPrime(int n);
+}
